46684a1bccdca996f0488657fac9ab22ae934aec
Ivy Zheng <ivyzheng@mit.edu>
2024-08-09 11:51:15 -0400

    changes to opt model generator from meeting 8/7

--- example.py
+++ example.py
@@ -2,18 +2,28 @@ import pyomo.environ as pyo
 from helpers import parsecase
 import pickle
 import main as ucml
+import case4gs
 
-db = parsecase(num_therm=2, num_nodes=4, num_lines=4, time_periods=3, num_scenarios = 1)
+ppc = case4gs.case4gs()
+db = parsecase(ppc, num_therm=2, num_nodes=4, num_lines=4, time_periods=3, num_scenarios = 1)
 data = pickle.loads(db)
 model = ucml.opt_model_generator(num_therm=2, num_nodes=4, num_lines=4, time_periods=3, num_scenarios=1)
 model.pprint()
+
+# single helper function so that the kwargs are the same
 
 instance = model.create_instance(data)
 
 opt = pyo.SolverFactory('gurobi')
 result = opt.solve(instance, tee=True)
 
-print("optimal value:", pyo.value(instance.cost))
 if hasattr(result.problem, 'upper_bound') and hasattr(result.problem, 'lower_bound'):
     primal_bound = result.problem.upper_bound
     dual_bound = result.problem.lower_bound
+    print("primal bound:", primal_bound)
+    print("dual bound:", dual_bound)
+
+print("optimal value:", pyo.value(instance.cost))
+
+out = {"dual_bound": dual_bound, "primal_bound": primal_bound}
+# make a file with output json file, with name
--- helpers.py
+++ helpers.py
@@ -13,32 +13,46 @@ def parsecase(ppc, num_solar=0, num_wind=0, num_batt=0, num_hydro=0, num_therm=0
     therm_start = hydro_start + num_hydro
     num_gen = num_solar + num_wind + num_batt + num_hydro + num_therm
 
-    p["RU"] = {therm_start + i : 0 for i in range(num_gen)}
-    p["RD"] = {therm_start + i : 0 for i in range(num_gen)}
-    p["Dd"] = {t : 10 for t in range(1, time_periods + 1)}
-    p["Fmax"] = {l : 0 for l in range(1, num_lines + 1)}
-    p["PTDF"] = {(n, l) : 10 for n in range(1, num_nodes+1) for l in range(1, num_lines+1)}
-    p["SU"] = {therm_start + i : 0 for i in range(num_gen)}
-    p["SD"] = {therm_start + i : 0 for i in range(num_gen)}
-    p["UT"] = {therm_start + i : 0 for i in range(num_gen)}
-    p["DT"] = {therm_start + i : 0 for i in range(num_gen)}
-    p["Rup"] = {t : 1 for t in range(1, time_periods + 1)}
-    p["Rdn"] = {t : 1 for t in range(1, time_periods + 1)}
-    p["Lg"] = {(g, i): 1 for g in range(1, num_gen + 1) for i in range(1, num_nodes + 1)}
-    p["Ld"] = {(d, i): 1 for d in range(1, num_demands + 1) for i in range(1, num_nodes + 1)}
-    p["Dl"] = {d : 1 for d in range(1, num_demands)}
+    k = .5 # calibrate
+
+    p["RU"] = {therm_start + i : ppc["gen"][i][8]/2 for i in range(num_gen)} # 1/2 of Pmax for thermal
+    p["RD"] = {therm_start + i : ppc["gen"][i][9]/2 for i in range(num_gen)}
+
+    Dsys = sum(ppc["bus"][n][2] for n in range(num_nodes))
+    p["Dd"] = {t : Dsys for t in range(1, time_periods + 1)} # Pd = demand for 1 hour
+
+    # nodal demand, Pd, N * T
+    # p["Dd"] = {(n, t) : ppc["bus"][n][2] for n in range(1, num_nodes + 1)for t in range(1, time_periods + 1)} # Pd = demand for 1 hour
+    
+    p["Fmax"] = {l : 0 for l in range(1, num_lines + 1)} # check that capacity is enough
+    # p["PTDF"] = {(n, l) : 10 for n in range(1, num_nodes+1) for l in range(1, num_lines+1)} # calculate from topology, not in constraint, remove
+    p["SU"] = {therm_start + i : k * ppc["gen"][i][8]/2 for i in range(num_gen)}
+    p["SD"] = {therm_start + i : k * ppc["gen"][i][9]/2 for i in range(num_gen)}
+    p["UT"] = {therm_start + i : 6 for i in range(num_gen)} # or 12
+    p["DT"] = {therm_start + i : 6 for i in range(num_gen)} # or 12
+
+    Psys = sum(ppc["gen"][i][9] for i in range (num_gen)) # sum Pmax
+    p["Rup"] = {t : .05 * Psys for t in range(1, time_periods + 1)} # 5% of demand
+    p["Rdn"] = {t : .05 * Psys for t in range(1, time_periods + 1)} 
+
+    p["Lg"] = {(g, i): 0 for g in range(1, num_gen + 1) for i in range(1, num_nodes + 1)} # fix
+    for g in range(1, num_gen + 1):
+        p["Lg"][(g, ppc["gen"][g - 1][0] + 1)] = 1
+
+    p["Ll"] = {(l, i): 0 for l in range(1, num_lines + 1) for i in range(1, num_nodes + 1)}
+    for l in range(1, num_lines + 1):
+        p["Ll"][(l, ppc["branch"][l-1][0]+1)] = 1 # from bus
+        p["Ll"][(l, ppc["branch"][l-1][1]+1)] = -1 # to bus
+
+    p["Ld"] = {(d, i): 0 for d in range(1, num_demands + 1) for i in range(1, num_nodes + 1)}
+    p["Dl"] = {d : 1 / num_demands for d in range(1, num_demands)} # remove, percentage of system load allocated to d
     
     p["Dt"] = {None: 1}
 
     p["Pmax"] = {therm_start + i : ppc["gen"][i][8] for i in range(num_gen)}
     p["Pmin"] = {therm_start + i : ppc["gen"][i][9] for i in range(num_gen)}
 
-    p["X"] = {i : ppc["branch"][i - 1][3] for i in range(1, num_lines + 1)}
-
-    p["Ll"] = {(l, n): 1 for l in range(1, num_lines + 1) for n in range(1, num_gen + 1)}
-
-    # for i in range(num_lines):
-    #     p["Ll"][(ppc["branch"][i][0], ppc["branch"][i][1])] = 1
+    p["X"] = {l : ppc["branch"][l - 1][3] for l in range(1, num_lines + 1)}
 
     if "gencost" in ppc:
         p["CSU"] = {therm_start + i : ppc["gencost"][i][1] for i in range(num_gen)}
